399392460caec11aeeba20876222d65af6fe5116
Shraddha Sharma <Shradz180191@users.noreply.github.com>
2021-04-27 21:59:24 -0500

    Update Box_sort.py

--- Box_sort.py
+++ Box_sort.py
@@ -3,3 +3,52 @@ Write a Box class whose init method takes three parameters and uses them to init
 
 Write a separate function named box_sort (not part of the Box class) that uses insertion sort to sort a list of Boxes from greatest volume to least volume.
 '''
+
+#Andrew Naegeli
+#CS162
+#Project-4b
+#April 26, 2021
+
+class Box():
+    def __init__(self, length, width, height):
+        self._length = length
+        self._width = width
+        self._height = height
+
+        def get_length(self):
+            return self._length
+        def get_width(self):
+            return self._width
+        def get_height(self):
+            return self._height
+        def get_volume(self):
+            return self._length*self._width*self._height
+
+
+
+def box_sort(box_list):
+  """
+  Sorts a_list in ascending order
+  """
+    box_volumes = []
+    for b in box_list:
+        box_volumes.append(b.get_volume())
+
+    for index in range(1, len(box_volumes)):
+        value = box_volumes[index]
+        pos = index - 1
+        while pos >= 0 and box_volumes[pos] > value:
+            box_volumes[pos + 1] = box_volumes[pos]
+            pos -= 1
+        box_volumes[pos + 1] = value
+
+    return box_volumes
+
+
+
+
+b1 = Box(3.4, 19.8, 2.1)
+b2 = Box(1.0, 1.0, 1.0)
+b3 = Box(8.2, 8.2, 4.5)
+box_list = [b1, b2, b3]
+box_sort(box_list)
