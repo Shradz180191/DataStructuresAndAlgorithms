a6c0af5cdcd91b6e154becda606483fc0a855340
Shraddha Sharma <Shradz180191@users.noreply.github.com>
2021-04-27 21:54:27 -0500

    Create Box_sort.py

---
+++ Box_sort.py
@@ -0,0 +1,5 @@
+'''
+Write a Box class whose init method takes three parameters and uses them to initialize the private length, width and height data members of a Box. It should also have a method named volume that returns the volume of the Box. It should have get methods named get_length, get_width, and get_height.
+
+Write a separate function named box_sort (not part of the Box class) that uses insertion sort to sort a list of Boxes from greatest volume to least volume.
+'''
