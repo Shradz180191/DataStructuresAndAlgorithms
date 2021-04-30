064038dae441fe21d1878999ebc58a1391f840b9
Shraddha Sharma <Shradz180191@users.noreply.github.com>
2021-04-30 12:03:11 -0500

    Create FantasyGameInventory.py

---
+++ FantasyGameInventory.py
@@ -0,0 +1,37 @@
+'''
+Create a “Fantasy Game Inventory” using a dictionary
+
+You are creating a fantasy video game.  The data structure to model the player’s inventory will be a dictionary where the keys are string values describing the item in the inventory and the value is an integer value of how many of that item the player has. For example,  {‘gold’: 1, ‘mana potions’: 3, ‘healing potion’: 6, ‘arrow’: 12, ‘dagger’: 1, ‘sword’: 3}.  This means the player has 1 gold, 3 mana potions, 6 healing potions,  12 arrows, 1 dagger and 3 sword.
+
+ 
+
+Write a function named displayInventory() that would take any possible inventory and display it like the following:
+
+Inventory:
+
+7 gold
+
+3 mana potions
+
+6 healing potions
+
+15 arrow
+
+1 dagger
+
+3 sword
+
+Total number of Items: 26
+
+ 
+
+When you loot something you want to add that information to your inventory.
+
+Define a sperate function, add_to_Inventory() that will open an loot variable and add it to the inventory.
+
+Troll Cave loot is 2 gold coins, 1 torch, 1 helm, 1 elven sword
+
+You can use the for loop to loop through all of the keys in the dictionary
+
+Your program should call the function from the main function
+'''
