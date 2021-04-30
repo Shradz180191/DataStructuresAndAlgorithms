7a9793648e3f4fd7f6a1a7a7e2c43c327c731d20
Shraddha Sharma <Shradz180191@users.noreply.github.com>
2021-04-30 11:51:23 -0500

    Update ceaser_cypher.py

--- ceaser_cypher.py
+++ ceaser_cypher.py
@@ -25,3 +25,46 @@ Hint: Use for to loop through each letter in the message.  Use the in method to 
 Remember to call the functions from the main function
 
 '''
+
+
+def encrypt_message(key,mess):
+    encoded = ""
+    for i in range(0,len(mess)):
+        val = ord(mess[i])
+        if(val + key > 126):
+            encoded += chr(val + key - 127 + 32)
+        else:
+            encoded += chr(val + key)
+    return encoded
+
+def decrypt)message(key,mess):
+    orgi = ""
+    for i in range(0,len(mess)):
+        val = ord(mess[i])
+        if (val - key < 32):
+            orgi += chr(((val - key )+ 127) - 32)
+        else:
+            orgi += chr(val - key)
+    return orgi
+
+def main():
+    choice = 'y'
+    while(choice == 'y'):
+        n = int(input("Enter 1 to encrypt, 2 to decode: "))
+        if(n==1):
+            mess = input("Enter the message to be encrypt: ")
+            key = int(input("Enter a key for encoding: "))
+            print("Message Encoded: \n\t"+encrypt_message(key,mess))
+            choice = input("\n\tContinue (y/n):\t")
+        elif(n==2):
+            mess = input("Enter an encrypted message to decode: ")
+            key = int(input("Enter a key for decoding: "))
+            print("Message Decoded: \n\t"+decrypt_message(key,mess))
+            choice = input("\n\tContinue (y/n):\t")
+        elif(n==0):
+            break
+
+if __name__ == "__main__":
+    main()
+  
+  
